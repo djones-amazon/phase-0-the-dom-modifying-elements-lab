762c25cc23aa822a20c7f7731e311e2f461b2a64
Devin Jones <devin.jones21083@gmail.com>
2023-04-09 21:05:20 -0400

    Edited index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Dr.Doom is the champion';
+document.body.append(newHeader);
